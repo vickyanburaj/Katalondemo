513f4fd62d7b6c2f16e589d780b18843675bfec3
Vignesh <Vignesh@Vignesh>
2023-07-03 18:06:51 +0530

    Initial comment for update MSID

---
+++ Scripts/UpdateMSID/Script1688387631944.groovy
@@ -0,0 +1,43 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.openBrowser('http://anladmin.okdollar.org/Account/Login')
+
+WebUI.acceptAlert()
+
+WebUI.setText(findTestObject('Object Repository/LogIn/Page_OK - Management Portal/input_Please Enter Your Information_txtUserName'),
+	'admin')
+
+WebUI.setEncryptedText(findTestObject('Object Repository/LogIn/Page_OK - Management Portal/input_Please Enter Your Information_txtPassword'),
+	'5mSEQ9MJzs63VALXlX2W8g==')
+
+WebUI.click(findTestObject('Object Repository/LogIn/Page_OK - Management Portal/span_Login'))
+
+WebUI.navigateToUrl('http://anladmin.okdollar.org/Dashboard/Dashboard')
+
+WebUI.click(findTestObject('Object Repository/LogIn/Page_OK/a_Device'))
+
+WebUI.click(findTestObject('Object Repository/LogIn/Page_OK/a_Simis  Msis'))
+
+WebUI.setText(findTestObject('Object Repository/LogIn/Page_OK/input_Phone Number_PhoneNumber'), '00959883769792')
+
+WebUI.sendKeys(findTestObject('Object Repository/LogIn/Page_OK/input_Phone Number_PhoneNumber'), Keys.chord(Keys.ENTER))
+
+WebUI.click(findTestObject('Object Repository/LogIn/Page_OK/span_Device'))
+
