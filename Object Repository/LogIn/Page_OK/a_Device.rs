3f35636816a5a093135a595e326385fe0ce92499
Vignesh <Vignesh@Vignesh>
2023-07-03 17:53:04 +0530

    Admin update MSID

---
+++ Object Repository/LogIn/Page_OK/a_Device.rs
@@ -0,0 +1,106 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>a_Device</name>
+   <tag></tag>
+   <elementGuidId>dca1a03d-0b52-43f0-a1cd-c3197c7310aa</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='sidebar']/ul/li[4]/a</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>li:nth-of-type(4) > a.dropdown-toggle</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>a</value>
+      <webElementGuid>14d46ae7-3ecb-480d-bfcc-a5a8aa8004b6</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>href</name>
+      <type>Main</type>
+      <value>#</value>
+      <webElementGuid>4c3c9472-d296-4699-ba18-206019313687</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>dropdown-toggle</value>
+      <webElementGuid>b918be35-753f-4a77-b40e-a904f8d92ff6</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+                        
+                          Device 
+
+                        
+                    </value>
+      <webElementGuid>dc872844-e77d-469b-8f2f-5ebf5851631b</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;sidebar&quot;)/ul[@class=&quot;nav nav-list&quot;]/li[4]/a[@class=&quot;dropdown-toggle&quot;]</value>
+      <webElementGuid>e510ec12-952a-4985-b830-4de5150b847f</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='sidebar']/ul/li[4]/a</value>
+      <webElementGuid>233b496f-0ca4-416e-aa11-53021c21d08d</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:href</name>
+      <type>Main</type>
+      <value>(//a[contains(@href, '#')])[4]</value>
+      <webElementGuid>6420d41c-da60-4a75-85d6-0dda4c672ebd</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//li[4]/a</value>
+      <webElementGuid>0cc16643-481b-47bd-97a1-fa721d617027</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//a[@href = '#' and (text() = '
+                        
+                          Device 
+
+                        
+                    ' or . = '
+                        
+                          Device 
+
+                        
+                    ')]</value>
+      <webElementGuid>a74012af-6fce-47b9-a55b-32ec74e5f003</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Object Repository/LogIn/Page_OK/a_Simis  Msis.rs
@@ -0,0 +1,116 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>a_Simis  Msis</name>
+   <tag></tag>
+   <elementGuidId>f655dd66-fa96-40fc-959f-6eca13c3feb8</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='sidebar']/ul/li[4]/ul[2]/li/a</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>a</value>
+      <webElementGuid>ac0582dc-f987-4363-ba6b-18012560ef9e</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>href</name>
+      <type>Main</type>
+      <value>/UserDetails/UpdateSimId</value>
+      <webElementGuid>af9c1022-897b-4056-a5bd-5da0c3eb6181</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+                                    
+                                    Simis &amp; Msis
+                                </value>
+      <webElementGuid>22fdf156-65a5-4549-b895-da46ad920d77</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;sidebar&quot;)/ul[@class=&quot;nav nav-list&quot;]/li[@class=&quot;open&quot;]/ul[@class=&quot;submenu&quot;]/li[1]/a[1]</value>
+      <webElementGuid>e5911713-eb1c-4712-83a2-5857e08e241f</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='sidebar']/ul/li[4]/ul[2]/li/a</value>
+      <webElementGuid>75409daa-8e8b-4802-8886-ea5730d9597e</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Device'])[1]/following::a[2]</value>
+      <webElementGuid>0632af10-256d-419e-b7a7-452ab7207b9d</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Manage Message'])[1]/preceding::a[2]</value>
+      <webElementGuid>1c04c7c3-363d-45e9-9514-00894ec97fd3</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Simis &amp; Msis']/parent::*</value>
+      <webElementGuid>ef28c42d-78cf-40fd-a89d-9000f76052dc</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:href</name>
+      <type>Main</type>
+      <value>//a[contains(@href, '/UserDetails/UpdateSimId')]</value>
+      <webElementGuid>0d63457e-8e93-429d-9ef6-7fc37e6d75da</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//li[4]/ul[2]/li/a</value>
+      <webElementGuid>f4ab0517-93bf-4c42-91bb-d9a9b6a1f4de</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//a[@href = '/UserDetails/UpdateSimId' and (text() = '
+                                    
+                                    Simis &amp; Msis
+                                ' or . = '
+                                    
+                                    Simis &amp; Msis
+                                ')]</value>
+      <webElementGuid>82cb62ef-4981-4712-8458-567f56125aa7</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
