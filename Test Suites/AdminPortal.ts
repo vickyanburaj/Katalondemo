ea5d299e6acacd1cda3aa3892871ff421e2a9763
Vignesh <Vignesh@Vignesh>
2023-07-06 11:46:54 +0530

    Added test suites

---
+++ Test Suites/AdminPortal.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>AdminPortal</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>c4a8d54c-bc67-4f7e-925f-39d6f1fdaa8f</testSuiteGuid>
+</TestSuiteEntity>
